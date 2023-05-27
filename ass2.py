048b96889cfa27d129f32cb085a566d8da47bd34
Akshit Murarka <101332366+Akshit-02@users.noreply.github.com>
2023-05-27 16:31:38 +0530

    Update ass2.py

--- ass2.py
+++ ass2.py
@@ -1,14 +1,19 @@
 # Assignment-1 (A)
+# Importing libraries
 import pandas as pd
 import csv
+
+# Opening the csv file
 data=pd.read_csv("users.csv")
 
 print("Data before sorting: ")
 print(data)
 
+# Sorting the users.csv file data according to FirstName
 data.sort_values("FirstName",inplace=True)
 
 print("Data after sorting: ")
 print(data)
 
+# Writing sorted data to users-sorted.csv file
 data.to_csv("users-sorted.csv")
