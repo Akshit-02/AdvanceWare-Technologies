8edf83f952a6e861d659741dde1c59b385a44d58
Akshit Murarka <101332366+Akshit-02@users.noreply.github.com>
2023-05-27 16:29:13 +0530

    Update ass1.py

--- ass1.py
+++ ass1.py
@@ -1,8 +1,10 @@
 # Assignment-1
+# Importing libraries
 import requests
 import time
 import csv
 
+# Creating/Opening the csv file
 with open('users.csv', 'a', newline='') as file:
   writer = csv.writer(file)
   writer.writerow(["id", "FirstName", "LastName", "Username", "Email", "Avatar", "Gender", "DoB", "Address"])
@@ -10,9 +12,11 @@ with open('users.csv', 'a', newline='') as file:
   counter=0
   
   while counter < 10:
+#   Making get request to the random api
     response = requests.get("https://random-data-api.com/api/v2/users")
     print(response.json())
     
+#   Appending the users data fetched from random api in csv file  
     data=response.json()
     add_dict=data['address']
     address=add_dict['street_name']+","+add_dict['street_address']+","+add_dict['city']+","+add_dict['state']+","+add_dict['country']+","+add_dict['zip_code']
@@ -20,4 +24,5 @@ with open('users.csv', 'a', newline='') as file:
 
     counter=counter+1
     
+#   Making get request every 1 sec
     time.sleep(1)
